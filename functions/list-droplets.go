d80c6d9d94ddf90855dbbce7912ee776b163278e
Luís Miranda <lantoniomiranda@ipvc.pt>
2024-06-06 19:25:56 +0100

    removed unused parameter

--- functions/list-droplets.go
+++ functions/list-droplets.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func ListDropletsHandler(w http.ResponseWriter, r *http.Request) {
+func ListDropletsHandler(w http.ResponseWriter) {
 	token := "dop_v1_11f72fc8c6223c25925ddeaaaca7f61da2ba8377fd9abb121d920812a90b5dc6"
 	output, err := RunTerraformListDroplets(token)
 	if err != nil {
