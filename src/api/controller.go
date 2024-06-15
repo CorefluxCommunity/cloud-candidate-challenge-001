bd39339b58082b7b49f591d98d45e5562077e169
Desgue <guedesgomero@gmail.com>
2024-06-15 11:41:34 +0100

    change api route name to conform rest standard

--- src/api/controller.go
+++ src/api/controller.go
@@ -9,22 +9,22 @@ type Controller struct {
 }
 
 func (c Controller) registerRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /health", c.HealthCheck)
-	mux.HandleFunc("GET /create", c.CreateHandler)
-	mux.HandleFunc("GET /destroy", c.DestroyHandler)
+	mux.HandleFunc("GET /droplet", c.HealthCheck)
+	mux.HandleFunc("POST /droplet", c.PostHandler)
+	mux.HandleFunc("DELETE /droplet", c.DeleteHandler)
 }
 func (c *Controller) HealthCheck(w http.ResponseWriter, _ *http.Request) {
 	io.WriteString(w, "Health check ok")
 }
 
-func (c *Controller) CreateHandler(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) PostHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello from Create")
 	// Recebe as configuraçoes para criar uma instancia no digital ocean em json e chama o serviço que se comunica com o terraform
 
 	// responde o output do terraform para o cliente
 }
 
-func (c *Controller) DestroyHandler(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) DeleteHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello from Destroy")
 	// recebe o ID do serviço que deseja remover
 
